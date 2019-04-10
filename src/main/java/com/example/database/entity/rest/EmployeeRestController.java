66b6f5555e8e42c3576c4f2fb3c2a1eb3dc055bb
SebastianB <seba23495@gmail.com>
2019-04-10 19:20:48 +0200

    Added upgrade/delete user

--- src/main/java/com/example/database/entity/rest/EmployeeRestController.java
+++ src/main/java/com/example/database/entity/rest/EmployeeRestController.java
@@ -5,9 +5,7 @@ import com.example.database.dao.EmployeeDAO;
 import com.example.database.entity.Employee;
 import com.example.database.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,7 +25,58 @@ public class EmployeeRestController {
    @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee==null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+
+    }
+
+    //add mapping for POST /employee - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+
+        //also just in case they pass an Id in Json set id to 0
+        //this is to force a save of new item .. instead of update
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+    //add mapping for put /employees - updating existing employee
+    @PutMapping("/employees")
+
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+    // add mapping for delete /employees/{employeeId}
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee =employeeService.findById(employeeId);
+        //throw exception if null
+        if(tempEmployee==null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee Id - " + employeeId;
+
+
+    }
+
 
 }
 
